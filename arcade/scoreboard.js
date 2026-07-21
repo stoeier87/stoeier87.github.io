43703559577cc4cedc7e55438a58ebc1891a3a52
Jesper Spiegelberg <jesperspiegelberg@gmail.com>
2026-07-21 12:27:17 +0200

    Update Firebase config and add new games
    Updated Firebase configuration with new credentials and added new games to the GAMES array.

--- arcade/scoreboard.js
+++ arcade/scoreboard.js
@@ -5,20 +5,21 @@ import {
 
 // Paste your Firebase config here
 const firebaseConfig = {
-  apiKey: "PASTE_ME",
-  authDomain: "PASTE_ME",
-  databaseURL: "PASTE_ME",
-  projectId: "PASTE_ME",
-  storageBucket: "PASTE_ME",
-  messagingSenderId: "PASTE_ME",
-  appId: "PASTE_ME"
+  apiKey: "AIzaSyDTebLTN13VnxG6tKoN5XsSk0OEg49Yz4s",
+  authDomain: "servicedesign-e1fe5.firebaseapp.com",
+  projectId: "servicedesign-e1fe5",
+  storageBucket: "servicedesign-e1fe5.firebasestorage.app",
+  messagingSenderId: "485465422440",
+  appId: "1:485465422440:web:2c8a02a1a9794b773419dd",
+  measurementId: "G-9M0GB4HHY0",
+  databaseURL: "https://servicedesign-e1fe5-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 // Add/edit games here
 const GAMES = [
-  { key: "pacman", label: "Pac-Man" },
-  { key: "tetris", label: "Tetris" },
-  { key: "snake", label: "Snake" }
+  { key: "orbit-runner", label: "Orbit Runner" },
+  { key: "moon-lander", label: "Moon Lander" },
+  { key: "iss-docking", label: "ISS Docking" }
 ];
 
 const app = initializeApp(firebaseConfig);
